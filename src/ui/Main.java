a60a019533bed80d4aab0865c0375314e83a174a
Servan42 <charlot.servan@gmail.com>
2018-09-14 11:17:22 +0200

    Adding a listenner to kill the program when the cross is clicked.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,14 +1,21 @@
 package ui;
 
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Main {
-	
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setTitle("RangeSlider");
 		frame.setSize(800, 600);
+
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
 		frame.getContentPane().add(new RangeSlider());
 		frame.setVisible(true);
 	}
