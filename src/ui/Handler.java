5a611611dfebcf17d3407a5948e67fe5948dbf7f
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-09-14 12:01:04 +0100

    Création du Handler -- Rien ne se passe pour l'instant

---
+++ src/ui/Handler.java
@@ -0,0 +1,48 @@
+package ui;
+
+import java.awt.event.MouseEvent;
+
+public class Handler {
+	private enum State { IDLE, HOLD, DRAG }
+	
+	private State state;
+	private RangeSlider rangeSlider;
+	
+	public void Handle(MouseEvent e) {
+		switch(state) {
+		case IDLE:
+			switch(e.getID()) {
+			case MouseEvent.MOUSE_PRESSED:
+				// TP BORNE CORRESPONDANTE + VALEUR
+				// ASSIGNER BORNE SAISIE
+				state = State.HOLD;
+				break;
+			default:;
+			}
+			break;
+		case HOLD:
+			switch(e.getID()) {
+			case MouseEvent.MOUSE_MOVED:
+				// BOUGER BORNE SAISIE + VALEUR
+				state = State.DRAG;
+				break;
+			case MouseEvent.MOUSE_RELEASED:
+				state = State.IDLE;
+			default:;
+			}
+			break;
+		case DRAG:
+			switch(e.getID()) {
+			case MouseEvent.MOUSE_MOVED:
+				// BOUGER BORNE SAISIE + VALEUR
+				break;
+			case MouseEvent.MOUSE_RELEASED:
+				state = State.IDLE;
+			default:;
+			}
+			break;
+		default:;
+		}
+	}
+	
+}
