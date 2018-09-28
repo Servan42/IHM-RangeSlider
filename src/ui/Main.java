f94c182c01ca6e7d28ab82c85d3569798d386701
Servan42 <charlot.servan@gmail.com>
2018-09-28 10:14:56 +0200

    WIP ; Building the interface

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,17 @@
 package ui;
 
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 
 public class Main {
 
@@ -16,11 +26,32 @@ public class Main {
 			}
 		});
 
-		try {
-			frame.getContentPane().add(new RangeSlider(0,10,5,6));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			JPanel p = new JPanel();
+			p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+			JLabel label = new JLabel("Test");
+			p.add(label);
+			label.setText("Coucou");
+			
+			try {
+				RangeSlider slider = new RangeSlider(0,30,10,20);
+				slider.addChangeListener(new ChangeListener() {
+					
+					public void stateChanged(ChangeEvent e) {
+						label.setText(new Integer(slider.getValue()).toString());
+						
+					}
+				});
+				
+				p.add(slider);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			frame.getContentPane().add(p);
+
+		
+		
 		frame.setVisible(true);
 	}
 }
